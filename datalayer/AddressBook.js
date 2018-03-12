22de8bd37f0e76b06591c561fba076b213e0782c
ksinton <kimsinton@gmail.com>
2018-03-11 21:34:23 -0700

    Updated error handling and added field validation

--- datalayer/AddressBook.js
+++ datalayer/AddressBook.js
@@ -1,18 +1,35 @@
 var Connection = require('./Connect.js');
+var Promise    = require('promise');
 
-Connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+  Connection.connect(function(err) {
+    if (err) {
+       console.log("ERROR: Unable to connect to mysql");
+     } else {
+       console.log("Connected");
+    }
+  });
 
 var AddressBook = function() {
 
     this.createUserAccount = function(name, username, password) {
 
-        var sql = "INSERT INTO user (name, username, password) VALUES ('"+ name +"', '"+ username +"', '"+ password +"')";
-        Connection.query(sql, function (err, result) {
-          if (err) throw err;
+      var sql = "INSERT INTO user (name, username, password) VALUES ("+ Connection.escape(name) +", "+ Connection.escape(username) +", "+ Connection.escape(password) +")";
+
+      var addUserPromise = new Promise(function (resolve, reject) {
+
+        Connection.query(sql, function (error, result) {
+          if (error) {
+            console.log(error);
+            reject(error)
+          } else {
+            resolve();
+          }
         });
+
+      });
+
+      return addUserPromise;
+
     }
 }
 
--- index.js
+++ index.js
@@ -14,6 +14,8 @@ addressBookObject = new AddressBook;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// var port = process.env.PORT || 4000;
+
 var router = express.Router();
 
 router.post('/user', function(req, res) {
@@ -22,30 +24,43 @@ router.post('/user', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
+    if (!name || (name.length < 1)) {
+      res.statusCode = 400;
+      res.json({ message: 'Unable to add user without name field' });
+    }
 
-    var addUserPromise = new Promise(function (resolve, reject) {
+    if (!username || (username.length < 4)) {
+      res.statusCode = 400;
+      res.json({ message: 'Unable to add user without valid username field.  User name must be at least 4 characters' });
+    }
 
-      try {
-        response = addressBookObject.createUserAccount(name, username, password);
-      }
-      catch(error) {
-        console.error(error);
-        reject(error);
-      }
+    if (username.length > 30) {
+      res.statusCode = 400;
+      res.json({ message: 'Unable to add user without valid username field.  User username must be no longer than 30 characters' });
+    }
 
-      resolve();
+    if (!password || (password.length < 6)) {
+      res.statusCode = 400;
+      res.json({ message: 'Unable to add user without valid password field.  User password must be at least 4 characters' });
+    }
 
-      });
+    if (password.length > 30) {
+      res.statusCode = 400;
+      res.json({ message: 'Unable to add user without valid password field.  User name must be no longer than 30 characters' });
+    }
 
-      addUserPromise.then(function() {
-        res.statusCode = 201;
-        res.json({ message: '1 record inserted 22' });
-      },function(error) {
-        res.statusCode = 500;
-        res.json({ message: 'An error occured:' + error });
+    var responsePromise = addressBookObject.createUserAccount(name, username, password);
+
+    responsePromise.then(function() {
+      res.statusCode = 201;
+      res.json({ message: '1 record inserted 22' });
+    },function(error) {
+      res.statusCode = 500;
+      res.json({ message: 'Error whiles saving the user record' });
     });
 
 });
+
 
 
 
